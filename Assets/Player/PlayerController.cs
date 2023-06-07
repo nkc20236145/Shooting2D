3877f23426e24d72ffbd7c364558e4d8c6c0dc13
nkc20236145 <nkc23.kisimoto.kousuke@gmail.com>
2023-06-07 15:15:34 +0900

    6/7更新

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -7,10 +7,12 @@ public class PlayerController : MonoBehaviour
 {
     Vector3 dir = Vector3.zero;   // 移動方向を保存する変数
     float speed = 5.0f;           // speedの値を保存
+    Animator anim;                // アニメーターコンポーネントの情報を
 
     void Start()
     {
-        
+        //  アニメーターコンポーネントの情報を保存
+        anim = GetComponent<Animator>();
     }
 
     void Update()
@@ -26,5 +28,20 @@ public class PlayerController : MonoBehaviour
         pos.x = Mathf.Clamp(pos.x, -8.5f, 8.5f);
         pos.y = Mathf.Clamp(pos.y, -4.75f, 4.75f);
         transform.position = pos;
+
+        // アニメーション設定
+        if(dir.y == 0)
+        {
+            // アニメーションクリップ[Player]を再生
+            anim.Play("Player");
+        }
+        else if(dir.y == 1)
+        {
+            anim.Play("Player_L");
+        }
+        else if (dir.y == -1)
+        {
+            anim.Play("Player_R");
+        }
     }
 }
